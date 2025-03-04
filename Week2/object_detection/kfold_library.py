ff7d19e1a0ae4a0c7fba356ab776480bd7b0f85b
markote <marco.terral@hotmail.com>
2025-03-04 13:56:16 +0100

    feat. Kfold update

--- Week2/object_detection/kfold_library.py
+++ Week2/object_detection/kfold_library.py
@@ -1,5 +1,18 @@
+import json
+import numpy as np 
+
 # need a sorted list of frame numbers and the path gt json
-frames = [x for x in range(2141)]
+
+def load_json(path_json):
+    try:
+        with open(path_json, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+        return data
+    except FileNotFoundError:
+        print(f"Error: File '{path_json}' not found.")
+    except json.JSONDecodeError:
+        print(f"Error: Could not decode JSON in file '{path_json}'.")
+    return None
 
 def save_json(path, json_struct):
     with open(path, "w") as json_file:
@@ -25,8 +38,8 @@ def generate_strategy_A(list_frames, path_json, K=4):
     partitions = np.array_split(list_frames, K)
     train_frames = set(partitions[0])
     eval_frames = set([y  for x in partitions[1:] for y in x])
-    train_gt_json_path = generate_gt("gt_train_strategy_A", train_frames, dict_gt)
-    eval_gt_json_path = generate_gt("gt_eval_strategy_A", eval_frames, dict_gt)
+    train_gt_json_path = generate_gt("gt_train_strategy_A", train_frames, json_dict)
+    eval_gt_json_path = generate_gt("gt_eval_strategy_A", eval_frames, json_dict)
 
     return [(eval_gt_json_path, eval_frames, train_gt_json_path, train_frames)]
 
@@ -38,8 +51,8 @@ def generate_strategy_B(list_frames, path_json, K=4):
         train_frames = set(partitions[i])
         tmp_partitions = partitions[:i] + partitions[i+1:]
         eval_frames = set([y for x in (tmp_partitions) for y in x])
-        train_gt_json_path = generate_gt(f"gt_train_strategy_B_partition_{i}", train_frames, dict_gt)
-        eval_gt_json_path = generate_gt(f"gt_eval_strategy_B_partition_{i}", eval_frames, dict_gt)
+        train_gt_json_path = generate_gt(f"gt_train_strategy_B_partition_{i}", train_frames, json_dict)
+        eval_gt_json_path = generate_gt(f"gt_eval_strategy_B_partition_{i}", eval_frames, json_dict)
         result.append((eval_gt_json_path, eval_frames, train_gt_json_path, train_frames))
 
     return result
@@ -52,8 +65,17 @@ def generate_strategy_C(list_frames, path_json, K=4):
         train_frames = set(partitions[i])
         tmp_partitions = partitions[:i] + partitions[i+1:]
         eval_frames = set([y for x in (tmp_partitions) for y in x])
-        train_gt_json_path = generate_gt(f"gt_train_strategy_C_partition_{i}", train_frames, dict_gt)
-        eval_gt_json_path = generate_gt(f"gt_eval_strategy_C_partition_{i}", eval_frames, dict_gt)
+        train_gt_json_path = generate_gt(f"gt_train_strategy_C_partition_{i}", train_frames, json_dict)
+        eval_gt_json_path = generate_gt(f"gt_eval_strategy_C_partition_{i}", eval_frames, json_dict)
         result.append((eval_gt_json_path, eval_frames, train_gt_json_path, train_frames))
 
     return result
+
+if __name__ == "__main__":
+    frames = [x for x in range(2141)]
+    strl= "./week2_anot.json"
+    generate_strategy_A(frames, strl)
+    generate_strategy_B(frames, strl)
+    generate_strategy_C(frames, strl)
+
+
